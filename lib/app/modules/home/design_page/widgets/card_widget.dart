4e92c700db39c274936a3079bf782265a9c3cf6e
Leticia Deodato <leticiamanuelletd@gmail.com>
2022-04-23 00:28:42 -0300

    Info cards widget

---
+++ lib/app/modules/home/design_page/widgets/card_widget.dart
@@ -0,0 +1,50 @@
+import 'dart:html';
+
+import 'package:create/app/modules/home/design_page/widgets/button_widget.dart';
+import 'package:flutter/material.dart';
+
+class CardWidget extends StatelessWidget {
+  const CardWidget(
+      {Key? key,
+      required this.description,
+      required this.image,
+      required this.title})
+      : super(key: key);
+  final String image;
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.shortestSide * 0.5,
+      width: MediaQuery.of(context).size.shortestSide * 0.4,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(this.image,
+              width: MediaQuery.of(context).size.shortestSide * 0.2),
+          Text(
+            this.title,
+            style: TextStyle(
+                color: Color(0xff660099),
+                fontSize: 20,
+                fontWeight: FontWeight.w900),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            this.description,
+            textAlign: TextAlign.center,
+          ),
+          ButtonWidget(
+              widget: Text('QUERO ESSA!'),
+              color: Color(0xfff23064),
+              width: MediaQuery.of(context).size.shortestSide * 0.3,
+              height: MediaQuery.of(context).size.shortestSide * 0.06,
+              borderRadius: 20)
+        ],
+      ),
+    );
+  }
+}
