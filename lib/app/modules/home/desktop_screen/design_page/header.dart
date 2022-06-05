b5988309f4ad20f5246127bca74806168e62ee10
Leticia Deodato <leticiamanuelletd@gmail.com>
2022-06-05 10:16:44 -0300

    Removing unnecessary widget

--- lib/app/modules/home/desktop_screen/design_page/header.dart
+++ lib/app/modules/home/desktop_screen/design_page/header.dart
@@ -1,4 +1,3 @@
-import 'package:create/app/modules/home/widgets/hyperlink_widget.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
